15202438841801d027849a276116f61a56161cd9
Yoo Zun <gkswns4869@daum.net>
2020-10-06 23:02:06 +0900

    Updates

---
+++ precache-manifest.7c1ddc27df28c9588a19808488d952af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33e28a742b9473b816f9a1bd3e1ee071",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "0b0b7bc934eeb6c2adfd",
+    "url": "/movie_app_2019/static/css/main.97a7826e.chunk.css"
+  },
+  {
+    "revision": "4d7c806d0e258b226914",
+    "url": "/movie_app_2019/static/js/2.00a09d16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2019/static/js/2.00a09d16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b0b7bc934eeb6c2adfd",
+    "url": "/movie_app_2019/static/js/main.f397fc2d.chunk.js"
+  },
+  {
+    "revision": "059a9ab2adfa282ae316",
+    "url": "/movie_app_2019/static/js/runtime-main.12568eff.js"
+  }
+]);
